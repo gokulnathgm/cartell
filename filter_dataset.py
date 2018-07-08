bd5098e72c171d54f983c8f7d697a708304f8418
Gokul Nath <gokulnathgm@gmail.com>
2018-07-08 12:07:24 +0530

    Added duplicates removal in filter dataset

--- filter_dataset.py
+++ filter_dataset.py
@@ -1,9 +1,12 @@
 import os
 
+import cv2
+import numpy as np
+
 index = {'mahindra_tuv': 1, 'honda_wrv': 2, 'toyota_yaris': 3, 'mahindra_xuv': 4, 'suzuki_alto': 5,
          'mahindra_scorpio': 6, 'tata_tiago': 7, 'tata_hexa': 8, 'suzuki_ciaz': 9, 'suzuki_baleno': 10,
          'ford_ecosport': 11, 'honda_amaze': 12, 'hyundai_creta': 13, 'volkswagen_vento': 14, 'toyota_innova': 15,
-         'honda_city': 16, 'hyundai_verna': 17, 'suzuki_ignis': 18, 'tata_nexon': 19}
+         'honda_city': 16, 'hyundai_verna': 17, 'suzuki_ignis': 18, 'tata_nexon': 19, 'mahindra_tuv': 20}
 
 directory_path = os.getcwd() + '/training_images/'
 
@@ -12,8 +15,6 @@ def rename_dataset():
     renamed_count = 0
     for category in index.keys():
         category_path = directory_path + category
-        print 'category_path: ', category_path, '\n\n\n'
-        print os.listdir(category_path)
         count = 0
         for filename in os.listdir(category_path):
             if filename.startswith(category):
@@ -32,4 +33,30 @@ def rename_dataset():
     print '.\n.\nRenamed {count} files'.format(count=renamed_count)
 
 
-rename_dataset()
+def is_similar(image1, image2):
+    return image1.shape == image2.shape and not (np.bitwise_xor(image1, image2).any())
+
+
+def remove_duplicates():
+    for category in index.keys():
+        print category, '...'
+        visited = []
+        cat_path = directory_path + category
+        files = os.listdir(cat_path)
+        for image1 in files:
+            filein = cat_path + '/' + image1
+            im1 = cv2.imread(filein)
+            visited.append(filein)
+            for image2 in files:
+                filecmp = cat_path + '/' + image2
+                if filecmp == filein or filecmp in visited:
+                    continue
+                visited.append(filecmp)
+                im2 = cv2.imread(filecmp)
+                if is_similar(im1, im2):
+                    print filein, '\n', filecmp, '\n'
+
+
+# rename_dataset()
+
+remove_duplicates()
