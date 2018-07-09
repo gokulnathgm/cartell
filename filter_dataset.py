7bf730c33adb700b2d141d9bf512cc3d4456547a
Gokul Nath <gokulnath@qburst.com>
2018-07-09 14:19:05 +0530

    Updated dataset

--- filter_dataset.py
+++ filter_dataset.py
@@ -6,7 +6,7 @@ import numpy as np
 index = {'mahindra_tuv': 1, 'honda_wrv': 2, 'toyota_yaris': 3, 'mahindra_xuv': 4, 'suzuki_alto': 5,
          'mahindra_scorpio': 6, 'tata_tiago': 7, 'tata_hexa': 8, 'suzuki_ciaz': 9, 'suzuki_baleno': 10,
          'ford_ecosport': 11, 'honda_amaze': 12, 'hyundai_creta': 13, 'volkswagen_vento': 14, 'toyota_innova': 15,
-         'honda_city': 16, 'hyundai_verna': 17, 'suzuki_ignis': 18, 'tata_nexon': 19, 'mahindra_tuv': 20}
+         'honda_city': 16, 'hyundai_verna': 17, 'suzuki_ignis': 18, 'tata_nexon': 19, 'toyota_etios': 20}
 
 directory_path = os.getcwd() + '/training_images/'
 
